5ffdec59ce7afadf1840ae125f09ae368c6e1c6f
Pawan <pawan.code@gmail.com>
2016-04-20 10:09:51 +0530

    gplus signin

--- donate/urls.py
+++ donate/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'donate.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'donate.views.index', name='home'),
+    url(r'^donate/', include('donation.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
